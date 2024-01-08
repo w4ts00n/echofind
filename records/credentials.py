cfbebfef6a9ae0a2f9fe4695e13344656331e941
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-08 17:15:18 +0100

    Formatted the code with black

--- records/credentials.py
+++ records/credentials.py
@@ -1,7 +1,7 @@
 # S3 Connection
-aws_access_key = 'AKIA4GJPM74NVTVORJHG'
-aws_secret_access_key = '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'
-bucket_name = 'mp4file-bucket'
+aws_access_key = "AKIA4GJPM74NVTVORJHG"
+aws_secret_access_key = "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"
+bucket_name = "mp4file-bucket"
 
 # ES Connection
 es_hosts = "https://b83912ca2fba48418d3e2b95f0ded52f.us-central1.gcp.cloud.es.io:443"
